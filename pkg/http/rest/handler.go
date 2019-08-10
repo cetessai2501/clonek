5afc8caed09e4c990a270f6fd751d4f45ff84f88
cetessai2501 <jules.lupus@laposte.net>
2019-08-10 23:24:24 +0200

    8 commit

--- pkg/http/rest/handler.go
+++ pkg/http/rest/handler.go
@@ -14,7 +14,7 @@ import (
 
 func Handler(a adding.Service, l listing.Service, r reviewing.Service) http.Handler {
 	router := httprouter.New()
-
+        router.GET("/", Index)
 	router.GET("/beers", getBeers(l))
 	router.GET("/beers/:id", getBeer(l))
 	router.GET("/beers/:id/reviews", getBeerReviews(l))
@@ -24,6 +24,13 @@ func Handler(a adding.Service, l listing.Service, r reviewing.Service) http.Hand
 
 	return router
 }
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    fmt.Fprint(w, "Welcome!\n")
+}
+
+
+
 
 // addBeer returns a handler for POST /beers requests
 func addBeer(s adding.Service) func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
