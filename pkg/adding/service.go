99ce209d05e7ff0acea0e74f140c3402020deff7
cetessai2501 <jules.lupus@laposte.net>
2019-08-10 22:19:45 +0200

    first commit

---
+++ pkg/adding/service.go
@@ -0,0 +1,49 @@
+package adding
+
+import (
+	"errors"
+)
+
+// ErrDuplicate is used when a beer already exists.
+var ErrDuplicate = errors.New("beer already exists")
+
+// Service provides beer adding operations.
+type Service interface {
+	AddBeer(...Beer)
+	AddSampleBeers([]Beer)
+}
+
+// Repository provides access to beer repository.
+type Repository interface {
+	// AddBeer saves a given beer to the repository.
+	AddBeer(Beer) error
+}
+
+type service struct {
+	bR Repository
+}
+
+// NewService creates an adding service with the necessary dependencies
+func NewService(r Repository) Service {
+	return &service{r}
+}
+
+// AddBeer adds the given beer(s) to the database
+func (s *service) AddBeer(b ...Beer) {
+
+	// any validation can be done here
+
+	for _, beer := range b {
+		_ = s.bR.AddBeer(beer) // error handling omitted for simplicity
+	}
+}
+
+// AddSampleBeers adds some sample beers to the database
+func (s *service) AddSampleBeers(b []Beer) {
+
+	// any validation can be done here
+
+	for _, bb := range b {
+		_ = s.bR.AddBeer(bb) // error handling omitted for simplicity
+	}
+}
